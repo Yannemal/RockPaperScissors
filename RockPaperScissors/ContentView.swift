9c66217685d2c8837645cdf585a34a8c274a22f9
yannemal <56878180+Yannemal@users.noreply.github.com>
2023-07-03 14:01:18 +0800

    implementing layout and basic data set up
    Screen for the game is now set up
    all the @State variables in place.
    
    NEXT: Methods

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -8,7 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: - Data
+    @State private var items = ["🧻", "🪨", "✂️"]
+    @State private var cpuChoice : Int = 0
+    @State private var cpuChosePaper = false
+    @State private var cpuChoseRock = false
+    @State private var cpuChoseScissors = false
+    @State private var winTowin : Bool = false
+    @State private var playerChoice : String = ""
+    @State private var playerChosePaper = false
+    @State private var playerChoseRock = false
+    @State private var playerChoseScissors = false
+    @State private var keepScore = ["⬛️", "⬛️", "⬛️", "⬛️", "⬛️", "⬛️", "⬛️", "⬛️", "⬛️", "⬛️"]
+    @State private var currentRound : Int = 0
+    
+    
     var body: some View {
+        
+        
+        // MARK: - VIEWDidLoad
         ZStack{
             RadialGradient(colors: [.blue, .black], center: .trailing, startRadius: 50, endRadius: 600)
                 .ignoresSafeArea()
@@ -18,30 +37,99 @@ struct ContentView: View {
                 .opacity(0.7)
             
             VStack {
-            
+                //currentRound:
+                Text(" ✿  ROUND \(currentRound )  ✿ ")
+                    .font(.system(size: 40))
+                    .bold()
+                    .foregroundColor(.gray)
+                    .padding(20)
+                // CPU section:
+                Text("CPU chooses : ")
+                    .font(.system(size: 30))
+                    .bold()
                 HStack{
-                    Text("🪨")
-                        .font(.system(size: 100))
-                        .rotationEffect(.degrees(180))
-                         .shadow(color: .white ,radius: 8)
-                         
-                    Text("🧻")
-                        .font(.system(size: 100))
-                    Text("✂️")
-                        .font(.system(size: 100))
+                    Text(items[0])
+                        .font(.system(size: 80))
+                        .shadow(color: .white, radius: cpuChosePaper ? 15 : 0)
+                    Text(items[1])
+                        .font(.system(size: 80))
+                        .shadow(color: .white, radius: cpuChoseRock ? 15 : 0)
+                    Text(items[2])
+                        .font(.system(size: 80))
                         .rotationEffect(.degrees(90))
+                        .shadow(color: .white, radius: cpuChoseScissors ? 15 : 0)
                     
                 }
-                Text("🏆 or ☹️")
-                    .font(.system(size: 100))
-                    .foregroundColor(.primary)
+                .padding(.bottom, 30)
+                
+                // modifier section:
+                Text("to win .. choose wisely : ")
+                    .font(.system(size: 30))
+                    .bold()
+                HStack{
+                    Text("🏆")
+                        .font(.system(size: 100))
+                        .foregroundColor(.primary)
+                        .shadow(color: .green, radius: winTowin ? 20 : 0)
+                    
+                    Text("or")
+                        .font(.system(size:80))
+                        .foregroundColor(.primary)
+                    Text("☹️")
+                        .font(.system(size: 80))
+                        .foregroundColor(.primary)
+                        .shadow(color: .red, radius: !winTowin ? 25 : 0)
+                }
+                
+                Text("player chooses : ")
+                    .font(.system(size: 30))
+                    .bold()
+                    .padding(.top, 30)
+                
+                HStack{
+                    Button(items[2]) {
+                        // action
+                    }   .font(.system(size: 80))
+                        .foregroundColor(.primary)
+                        .rotationEffect(.degrees(180))
+                        .shadow(color: .white, radius: playerChoseScissors ? 25 : 0)
+                    Button(items[1]) {
+                        // action
+                    }   .font(.system(size: 80))
+                        .foregroundColor(.primary)
+                        .rotationEffect(.degrees(180))
+                        .shadow(color: .white, radius: playerChoseRock ? 25 : 0)
+
+                    Button(items[0]) {
+                        // action
+                    }   .font(.system(size: 80))
+                        .foregroundColor(.primary)
+                        .rotationEffect(.degrees(90))
+                        .shadow(color: .white, radius: playerChosePaper ? 25 : 0)
+
+                }
+                Spacer()
+                // keep score section:
+                HStack{
+                    ForEach(0..<keepScore.count, id: \.self) {
+                        Text("\(keepScore[$0])")
+
+                    }
+
+                }
                 
             }
-            .padding()
         }
     }
-}
 
+// MARK: - METHODS
+    func itemTapped(choice: Int){
+                //code for playerChoosing
+            }
+        
+    
+}
+// MARK: - PREVIEW
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
